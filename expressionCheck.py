451abfbaabbacfdf4535d2988241a59db60520dd
naufal <naufalfirdaus21@gmail.com>
2022-11-25 15:35:29 +0700

    add string expression

--- expressionCheck.py
+++ expressionCheck.py
@@ -29,7 +29,7 @@ def isOperator(word):
 
 
 def isOperan(word):
-    return word == "_variable_" or word == "_number_" or word == "true" or word == "false" or word == "null"
+    return word == "_variable_" or word == "_number_" or word == "true" or word == "false" or word == "null" or word == "_string_"
 
 
 def isPrefixOp(word):
@@ -210,121 +210,6 @@ def expressionCheck(matofword):
     return matofword, valid, lineError
 
 # FOR DEBUGGING FROM FILE INPUT
-
-# def ReadyToParse(file):
-#     # Mengubah isi file menjadi array of sentence tiap baris
-#     file = file.split('\n')
-#     arr = []
-#     # Proses tiap baris mengubah multiline comments menjadi _comment_
-#     found = False
-#     for line in file:
-#         if (globalVariable.acc):
-#             # Mengubah semicolon menjadi _semicolon
-#             line = line.replace(';', ' _semicolon_')
-#             # Mengubah multiline comments menjadi _comment_
-#             idxMultiOpen = line.find('/*')
-#             if (idxMultiOpen != -1 or found):
-#                 found = True
-#                 idxMultiClose = line.find('*/')
-#                 if (idxMultiClose == -1):
-#                     line = line.replace(line[0:], '_comment_')
-#                 else:
-#                     line = line.replace(line[0:], '_comment_')
-#                     found = False
-#             else:  # idxMultiOpen == -1 and not found
-#                 idxMultiClose = line.find('*/')
-#                 if (idxMultiClose == -1):
-#                     found = False
-#                 else:
-#                     globalVariable.acc = False
-
-#             # Mengubah singleline comments menjadi _comment_
-#             if (globalVariable.acc):
-#                 idxSingle = line.find('//')
-#                 if (idxSingle != -1):
-#                     line = line.replace(
-#                         line[0:], "_comment_")
-
-#             # Mengubah bracket
-#             for brackets in globalVariable.js_brackets:
-#                 if (brackets == '('):
-#                     line = line.replace(brackets, ' _commonOpen_ ')
-#                 if (brackets == ')'):
-#                     line = line.replace(brackets, ' _commonClose_')
-#                 if (brackets == '{'):
-#                     line = line.replace(brackets, ' _curlyOpen_')
-#                 if (brackets == '}'):
-#                     line = line.replace(brackets, ' _curlyClose_')
-#                 if (brackets == '['):
-#                     line = line.replace(brackets, ' _squareOpen_')
-#                 if (brackets == ']'):
-#                     line = line.replace(brackets, ' _squareClose_')
-
-#             # Mengubah keyword operator
-#             for operator in globalVariable.js_compareOp:
-#                 line = line.replace(operator, ' _compare_')
-#             for operator in globalVariable.js_assignLogicOp:
-#                 line = line.replace(operator, ' _assign_')
-#             for operator in globalVariable.js_logicOp:
-#                 line = line.replace(operator, ' _logic_')
-#             for operator in globalVariable.js_assignBitwiseOp:
-#                 line = line.replace(operator, ' _assign_')
-#             for operator in globalVariable.js_bitwiseOp:
-#                 line = line.replace(operator, ' _bitwise_')
-#             for operator in globalVariable.js_assignArithOp:
-#                 line = line.replace(operator, ' _assign_')
-#             for operator in globalVariable.js_arithOp:
-#                 line = line.replace(operator, ' _arith_')
-#             for operator in globalVariable.js_assignDeclare :
-#                 line = line.replace(operator, ' _equalSign_')
-
-#             # Mengubah tipe data
-#             # Mengubah keyword number menjadi '_string_'
-#             idxStringOpen = line.find("'")
-#             if (idxStringOpen != -1):
-#                 idxStringClose = line.find("'", idxStringOpen+1)
-#                 if (idxStringClose == -1):
-#                     globalVariable.acc = False
-#                 else:
-#                     line = line.replace(
-#                         line[idxStringOpen:idxStringClose+1], ' _string_')
-#             else:  # idxstringOpen == -1
-#                 idxStringClose = line.find("'")
-#                 if (idxStringClose != -1):
-#                     globalVariable.acc = False
-
-#             idxStringOpen = line.find('"')
-#             if (idxStringOpen != -1):
-#                 idxStringClose = line.find('"', idxStringOpen+1)
-#                 if (idxStringClose == -1):
-#                     globalVariable.acc = False
-#                 else:
-#                     line = line.replace(
-#                         line[idxStringOpen:idxStringClose+1], ' _string_')
-#             else:  # idxstringOpen == -1
-#                 idxStringClose = line.find('"')
-#                 if (idxStringClose != -1):
-#                     globalVariable.acc = False
-
-#             # Mengubah keyword object menjadi '_object_'
-#             # ''' BELOM NIH '''
-
-#             # Mengubah isi file menjadi array of array of word
-#             if (line != ''):
-#                 line = line.split()
-
-#             # Mengubah keyword number menjadi '_number_'
-#             line = validNumber.isNumber(line)
-
-#             # Mengubah kata yang tidak ada dalam keyword menjadi _variable_
-#             if (globalVariable.acc):
-#                 line = validVariable.isVariable(
-#                     line, globalVariable.replacedsymbol, globalVariable.js_grammar)
-
-#             arr.append(line)
-#             globalVariable.rowError += 1
-#     return arr
-
 
 # dir = 'test/' + str('inputAcc.js')
 # file = open(dir, "r")
